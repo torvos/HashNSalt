d4128ebf99ce684ef1d09ab186d2fd04a3d2c0b5
Philippe Thibault <philippet@gmail.com>
2015-08-12 10:21:16 -0400

    Basic Salt and Hash demo in C# V4.5.2

---
+++ HashNSalt(C_Sharp V4_5_2)/HashNSalt(C_Sharp V4_5_2)/HashNSalt.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HashNSalt.aspx.cs" Inherits="HashNSalt_C_Sharp_V4_5_2_.HashNSalt" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblPassword" runat="server" Text="Enter some text:"></asp:Label><asp:TextBox ID="txtPassword" runat="server"></asp:TextBox> 
+            <br />
+            <br />
+            <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+            <br />
+            <br />
+            <asp:Label ID="lblSalt" runat="server" Text=""></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="lblHash" runat="server" Text=""></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="lblValidate" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ HashNSalt(C_Sharp V4_5_2)/HashNSalt(C_Sharp V4_5_2)/HashNSalt.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Text;
+using System.Security.Cryptography;
+
+
+namespace HashNSalt_C_Sharp_V4_5_2_
+{
+    public partial class HashNSalt : System.Web.UI.Page
+    {
+        // The following constants may be changed without breaking existing hashes.
+        public const int SALT_BYTE_SIZE = 24;
+        public const int HASH_BYTE_SIZE = 24;
+        public const int PBKDF2_ITERATIONS = 1000;
+
+        public const int ITERATION_INDEX = 0;
+        public const int SALT_INDEX = 1;
+        public const int PBKDF2_INDEX = 2;
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            
+            lblSalt.Text = "Generating Salt";
+
+            RNGCryptoServiceProvider csprng = new RNGCryptoServiceProvider();
+            byte[] salt = new byte[SALT_BYTE_SIZE];
+            csprng.GetBytes(salt);
+
+            lblSalt.Text = "Salt Generated: " + Convert.ToBase64String(salt);
+            lblHash.Text = "Hashing the text with the salt";
+
+            Rfc2898DeriveBytes pbkdf2 = new Rfc2898DeriveBytes(txtPassword.Text, salt);
+            pbkdf2.IterationCount = PBKDF2_ITERATIONS;
+            byte[] hash = pbkdf2.GetBytes(HASH_BYTE_SIZE);
+
+            lblHash.Text = "Hashed text: " + Convert.ToBase64String(hash);
+
+            lblValidate.Text = "Now you would store the hash and salt, and it's advisable that you store the interations in your database.<br/>To validate when someone logs in you take the salt and hash stored for that user, take the password<br/>they entered and re-hash it with the same salt as your stored hash...<br/>you should now have two identical hashes... comapre them with something like:<br/><br/>uint diff = (uint)OriginalHash.Length ^ (uint)NewHash.Length;<br/>for (int i = 0; i < OriginalHash.Length && i < NewHash.Length; i++)<br/>&nbsp;&nbsp;&nbsp;diff |= (uint)(OriginalHash[i] ^ NewHash[i]);< br />";
+        }
+
+    }
+}
